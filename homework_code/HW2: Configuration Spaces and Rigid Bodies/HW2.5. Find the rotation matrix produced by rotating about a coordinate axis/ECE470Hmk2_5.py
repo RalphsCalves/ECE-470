902922677e360c8cc9eef72524c621a930f47384
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 13:29:22 -0600

    Create ECE470Hmk2_5.py

---
+++ homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.5. Find the rotation matrix produced by rotating about a coordinate axis/ECE470Hmk2_5.py
@@ -0,0 +1,14 @@
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# Suppose that frame 1 and frame 0 are initially aligned, and that frame 1 rotates by an angle  about the  axis.
+
+# ====== Enter INPUT HERE =======
+theta = 1.89
+
+# =========== ANSWER ============
+# Compute R_01:
+
+
+# ============ CODE =============
