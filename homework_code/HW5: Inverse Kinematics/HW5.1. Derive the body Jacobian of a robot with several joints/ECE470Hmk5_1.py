e855cc25ca6cafd1f08ddda2e0cc557949cf6d04
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 15:31:05 -0600

    Update ECE470Hmk5_1.py

--- homework_code/HW5: Inverse Kinematics/HW5.1. Derive the body Jacobian of a robot with several joints/ECE470Hmk5_1.py
+++ homework_code/HW5: Inverse Kinematics/HW5.1. Derive the body Jacobian of a robot with several joints/ECE470Hmk5_1.py
@@ -1,1 +1,17 @@
-asd
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# The schematic of a robot with 3 joints (in the zero position) is shown above. 
+# Frame 0 is fixed to the base. Frame 1 is fixed to the tool. 
+# For the given values of the joint variables theta
+
+# ====== Enter INPUT HERE =======
+theta = np.array([[-0.74], [0.83], [0.71]])
+
+# =========== ANSWER ============
+# find the matrix (J) for which the body twist of frame 1 can be expressed as
+# This matrix is called the "body Jacobian" of the robot.
+
+
+# ============ CODE =============
