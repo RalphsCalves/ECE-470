9a619e3089f00844c52a8ecb313c30c4285bc6f1
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 13:16:24 -0600

    Update ECE470Hmk2_4.py

--- homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.4. Find the coordinates of a point in a different frame/ECE470Hmk2_4.py
+++ homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.4. Find the coordinates of a point in a different frame/ECE470Hmk2_4.py
@@ -1,1 +1,19 @@
+# AUTHOR: Ralph Balita
+import numpy as np
 
+# ========== QUESTION: ==========
+# The position and orientation of frame 2 in the coordinates of frame 3 are:
+# A point  in the coordinates of frame 3 is:
+
+# ====== Enter INPUT HERE =======
+p_2 = np.array([[0.31876124], [0.89446281], [0.74136116]])
+R_32 = np.array([[-0.34524714, -0.46158489, -0.81715592], 
+                 [-0.35024074, -0.74442308, 0.56847665], 
+                 [-0.87070996, 0.48246623, 0.09534410]])
+q_3 = np.array([[0.86272200], [0.51057961], [0.43060015]])
+
+# =========== ANSWER ============
+# Find  in the coordinates of frame 2:
+
+
+# ============ CODE =============
