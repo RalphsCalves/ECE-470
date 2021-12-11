5742eab1adc0f31c075890e0a43596bb2906f510
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 13:31:53 -0600

    Create ECE470Hmk2_6.py

---
+++ homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.6. Find the inverse transformation/ECE470Hmk2_6.py
@@ -0,0 +1,17 @@
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# The pose of frame 2 in the coordinates of frame 0 is:
+
+# ====== Enter INPUT HERE =======
+T_02 = np.array([[0.79372387, 0.11329095, 0.59763499, -0.89605466], 
+                 [-0.22588542, 0.96714292, 0.11666338, -0.14768354], 
+                 [-0.56478155, -0.22759554, 0.79323520, 0.16004298], 
+                 [0.00000000, 0.00000000, 0.00000000, 1.00000000]])
+
+# =========== ANSWER ============
+# Find the pose of frame 0 in the coordinates of frame 2: T_20
+
+
+# ============ CODE =============
