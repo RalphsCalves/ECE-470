a474356679bd0540ff033fb0ea4458191ae9ddda
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 13:13:26 -0600

    Create ECE470Hmk2_3.py

---
+++ homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.3. Find the coordinates of a point in a different frame/ECE470Hmk2_3.py
@@ -0,0 +1,16 @@
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# The pose of frame 2 in the coordinates of frame 5 is:
+# A point  in the coordinates of frame 2 is:
+
+# ====== Enter INPUT HERE =======
+T_52 = np.array([[-0.14537865, -0.02301517, 0.98910836, -0.41881369], [0.87942412, 0.45503461, 0.13984534, 0.97198259], [-0.45329710, 0.89017628, -0.04591222, -0.78401912], [0.00000000, 0.00000000, 0.00000000, 1.00000000]])
+p_2 = np.array([[0.30219896], [0.72020766], [-0.19185891]])
+
+# =========== ANSWER ============
+# Find  in the coordinates of frame 5:
+
+
+# ============ CODE =============
