36e63d2fef59598c65370504f83c13d0caca0ad4
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 15:34:54 -0600

    Update ECE470Hmk5_2.py

--- homework_code/HW5: Inverse Kinematics/HW5.2. Find a set of joint variables that would produce a given tool pose for a 6-DOF robot/ECE470Hmk5_2.py
+++ homework_code/HW5: Inverse Kinematics/HW5.2. Find a set of joint variables that would produce a given tool pose for a 6-DOF robot/ECE470Hmk5_2.py
@@ -1,1 +1,18 @@
-as
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# The schematic of a robot with 6 joints (in the zero position) is shown above. 
+# Frame 0 is fixed to the base. Frame 1 is fixed to the tool. 
+
+# ====== Enter INPUT HERE =======
+T_1in0 = np.array([[-0.45174805, -0.17916214, -0.87397061, 0.60710123], 
+                   [0.42990787, -0.90210250, -0.03728664, 4.05579210], 
+                   [-0.78173072, -0.39257101, 0.48454626, -3.77935434], 
+                   [0.00000000, 0.00000000, 0.00000000, 1.00000000]])
+
+# =========== ANSWER ============
+# Find a set of joint variables theta that would result in the given pose T_01
+
+
+# ============ CODE =============
