c7de12417a9d9727f9e7e768ac186bdec3a859e8
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-11 14:42:46 -0600

    Update ECE470Hmk3_4.py

--- homework_code/HW3: Forward Kinematics/HW3.4. Find the pose of the tool frame for a robot with several joints/ECE470Hmk3_4.py
+++ homework_code/HW3: Forward Kinematics/HW3.4. Find the pose of the tool frame for a robot with several joints/ECE470Hmk3_4.py
@@ -1,1 +1,16 @@
-sd
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# The schematic of a robot with 5 joints (in the zero position) is shown above. 
+# Frame 0 is fixed to the base. Frame 1 is fixed to the tool. 
+# Denote the joint variables together by theta. 
+
+# ====== Enter INPUT HERE =======
+theta = [-0.47 0.09 0.46 0.57 -0.52];
+
+# =========== ANSWER ============
+# Find the pose  of frame 1 when the joint variables take the given values.
+
+
+# ============ CODE =============
