041fba871189462fa0df95e02ff708319ecf3320
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-12 07:02:47 -0600

    Create ECE470Hmk7_2.py

---
+++ homework_code/HW7: Trajectory Generation and Motion Planning/HW7.2. Trajectory generation/ECE470Hmk7_2.py
@@ -0,0 +1,17 @@
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# We want a robot to move from one location to another location in a straight line with time T = 5. 
+# Let's parameterize the path as follows: 
+# s = a_0 + a_1(t) + a_2(t^2) + a_3(t^3)
+# where time t belongs to [0,T]. 
+
+
+# ====== Enter INPUT HERE =======
+NO INPUTS NO CODE
+
+# =========== ANSWER ============
+# We need the robot to start with zero velocity, and reach the destination with zero velocity. Find a_0, a_1, a_2, a_3.
+
+# ============ CODE =============
