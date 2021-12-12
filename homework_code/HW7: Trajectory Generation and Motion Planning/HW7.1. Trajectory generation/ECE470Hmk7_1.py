02b6c5ddabdf9d333479983388b0b6016e6b2fc1
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-12 06:58:51 -0600

    Update ECE470Hmk7_1.py

--- homework_code/HW7: Trajectory Generation and Motion Planning/HW7.1. Trajectory generation/ECE470Hmk7_1.py
+++ homework_code/HW7: Trajectory Generation and Motion Planning/HW7.1. Trajectory generation/ECE470Hmk7_1.py
@@ -1,1 +1,17 @@
-sf
+# AUTHOR: Ralph Balita
+import numpy as np
+
+# ========== QUESTION: ==========
+# As shown in the figure above, consider an elliptical path in the  plane. 
+# The path starts at origin and proceeds clockwise to , , , and back to origin. 
+
+
+# ====== Enter INPUT HERE =======
+x = 2.00000000
+y = 3.00000000
+s = 0.91000000
+
+# =========== ANSWER ============
+# First write the path as a function of path parameter  then find the xy-coordinate of a point x_1, y_1　on this path if s = .91.
+
+# ============ CODE =============
