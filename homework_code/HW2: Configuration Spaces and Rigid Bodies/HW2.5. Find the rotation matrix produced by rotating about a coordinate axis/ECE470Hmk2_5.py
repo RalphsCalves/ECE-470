bd8a393c30c965195465d4bc6eb28f9be6b3adf9
Ralph <32446528+RalphsCalves@users.noreply.github.com>
2021-12-16 05:22:43 -0600

    Update ECE470Hmk2_5.py

--- homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.5. Find the rotation matrix produced by rotating about a coordinate axis/ECE470Hmk2_5.py
+++ homework_code/HW2: Configuration Spaces and Rigid Bodies/HW2.5. Find the rotation matrix produced by rotating about a coordinate axis/ECE470Hmk2_5.py
@@ -2,13 +2,22 @@
 import numpy as np
 
 # ========== QUESTION: ==========
-# Suppose that frame 1 and frame 0 are initially aligned, and that frame 1 rotates by an angle  about the  axis.
+# Suppose that frame 1 and frame 0 are initially aligned, and that frame 1 rotates by an angle theta about the z_0 axis.
 
 # ====== Enter INPUT HERE =======
-theta = 1.89
+theta = 0.76 # about z_0 axis
+
+theta = -1.51 # about x_0 axis
 
 # =========== ANSWER ============
 # Compute R_01:
+# R_01 = [[0.724836010741, -0.688921445111, 0], [0.688921445111, 0.724836010741, 0], [0, 0, 1]]
+
+# Compute R_01
+# R_01 = [[1, 0, 0], [0, 0.0607588812194, 0.998152472498], [0, -0.998152472498, 0.0607588812194]]
+
+
+
 
 
 # ============ CODE =============
